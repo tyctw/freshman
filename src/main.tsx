88489c1df13609b37db5a22ad25b1e426ff15106
tyctw <tyctw.analyze@gmail.com>
2026-07-09 00:15:52 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,6 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// 處理 GitHub Pages 404 重新導向
-if (sessionStorage.redirect) {
-  const redirect = sessionStorage.redirect;
-  delete sessionStorage.redirect;
-  if (redirect !== window.location.href) {
-    window.history.replaceState(null, null, redirect);
-  }
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
